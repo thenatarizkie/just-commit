c30fbfa34c8137eacf76b170eb2cf753bd8adb0e
thenatarizkie <natarizkie7@gmail.com>
2025-01-12 21:19:42 +0700

    chore: archive old branches that are no longer in use

---
+++ src/utils.js
@@ -0,0 +1,35 @@
+import { execSync } from 'child_process';
+import { execa } from 'execa';
+
+export async function getDiffForFile(file) {
+    const { stdout: diff } = await execa('git', ['diff', '--staged', '--', file]);
+    return diff;
+}
+
+export function getGitChanges() {
+    const gitDiff = execSync('git diff --name-only').toString().trim();
+
+    if (gitDiff) {
+        return gitDiff.split('\n');
+    } else {
+        const gitStatus = execSync('git status --porcelain').toString().trim();
+        const listDiff = [];
+
+        gitStatus.split('\n').forEach((line) => {
+            const [status, file] = line.trim().split(' ');
+            if (status === 'M' || status === '??') {
+                listDiff.push(file);
+            }
+        });
+
+        return listDiff;
+    }
+}
+
+export function stageFile(file) {
+    execSync('git add ' + file, { stdio: 'inherit' });
+}
+
+export function commitFile(commitMessage) {
+    execSync(`git commit -m "${commitMessage}"`, { stdio: 'inherit' });
+}
