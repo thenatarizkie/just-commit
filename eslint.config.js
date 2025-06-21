fd5a613261acd3089b91b48e752485527162a324
thenatarizkie <natarizkie7@gmail.com>
2025-06-21 19:09:45 +0700

    fix: configure eslint to enforce import sorting using `eslint-plugin-simple-import-sort

--- eslint.config.js
+++ eslint.config.js
@@ -1,5 +1,22 @@
-import globals from 'globals';
 import pluginJs from '@eslint/js';
+import simpleImportSort from 'eslint-plugin-simple-import-sort';
+import globals from 'globals';
 
 /** @type {import('eslint').Linter.Config[]} */
-export default [{ languageOptions: { globals: globals.browser } }, pluginJs.configs.recommended];
+export default [
+    {
+        languageOptions: {
+            ecmaVersion: 2022,
+            sourceType: 'module',
+            globals: globals.node,
+        },
+        plugins: {
+            'simple-import-sort': simpleImportSort,
+        },
+        rules: {
+            'simple-import-sort/imports': 'error',
+            'simple-import-sort/exports': 'error',
+        },
+    },
+    pluginJs.configs.recommended,
+];
